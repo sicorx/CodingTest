86e9e9a190bd76b82c6d92870c9255b8ea407d54
sicorx <sicorx@gmail.com>
2023-09-08 01:01:03 +0900

    [unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -2,6 +2,6 @@ def solution(todo_list, finished):
     answer = []
     
     for i in range(len(todo_list)) :
-        if finished[i] == 0 :
+        if finished[i] == False :
             answer.append(todo_list[i])
     return answer
