c3e98a6563763cee5520de21ad0846035c5053e3
sicorx <sicorx@gmail.com>
2023-09-09 03:45:21 +0900

    [unrated] Title: x 사이의 개수, Time: 2.14 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = []
+    
+    answer = myString.split('x')
+    
+    for i in range(len(answer)) :
+        answer[i] = len(answer[i])
+        
+    return answer
